a5ba8ba5ec2c136038f585040298bae1c5b8259e
cloverstd <cloverstd@gmail.com>
2014-06-26 23:06:26 +0800

    fix error

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 function Class(params) {
-  if (params.initialize === undefined) {
-    var b = function() {
-    };
-    return b;
-  } else {
-    var f = function() {
-      // f is a constructor
-      params.initialize.apply(this, arguments);
-    };
-    // f.constructor === f // true
+  var f;
 
-    for (var key in params) {
-      if (key !== "initialize") {
-        f.prototype[key] = params[key];
-      }
-    }
-    return f;
+  if (params === undefined) {
+    f = function() {};
+  } else {
+    f = params.initialize ? params.initialize : function() {};
   }
+
+  for (var key in params) {
+    if (key !== "initialize") {
+      f.prototype[key] = params[key];
+    }
+  }
+
+  return f;
 }
+
+
 module.exports = Class;
