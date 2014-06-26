0f8eee87717ae6c83ef5a10bb05de3f4699655a1
cloverstd <cloverstd@gmail.com>
2014-06-26 20:48:16 +0800

    implement instance methods

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ function Class(params) {
       params.initialize.apply(this, arguments);
     };
     // f.constructor === f // true
+
+    for (var key in params) {
+      if (key !== "initialize") {
+        f.prototype[key] = params[key];
+      }
+    }
     return f;
   }
 }
-
 module.exports = Class;
